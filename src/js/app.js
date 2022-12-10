b296219c7556cc96b315eabf28aa2e65a0556b55
Simeodey <adamand12@abv.bg>
2022-12-10 09:32:08 +0200

    solution

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,16 @@
-import "../scss/app.scss";
-
-window.addEventListener("DOMContentLoaded", () => {
+import '../scss/app.scss';
+import * as ramda from 'ramda';
+window.addEventListener('DOMContentLoaded', () => {
   // This block will be executed once the page is loaded and ready
 
   const arrayToPluck = [
-    { name: "John", class: "is-primary" },
-    { age: 23, class: "is-warning" },
-    { job: "programmer", class: "is-danger" },
+    { name: 'John', class: 'is-primary' },
+    { age: 23, class: 'is-warning' },
+    { job: 'programmer', class: 'is-danger' },
   ];
-  const articles = document.querySelectorAll("article");
+  const articles = document.querySelectorAll('article');
+  const classes = ramda.pluck('class', arrayToPluck);
+  articles.forEach((el, index) => {
+    el.classList.add(classes[index]);
+  });
 });
